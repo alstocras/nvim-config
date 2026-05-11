f7ee423df4d1459b5b941876aadb767e3dce60e0
Alstocras <alstoperiment@gmail.com>
2026-05-11 14:18:56 +0000

    date and time plugin

--- init.lua
+++ init.lua
@@ -12,3 +12,10 @@ vim.cmd("colorscheme onedark")
 -- Let VimTeX handle LaTeX the old-fashioned way (it's more stable)
 vim.g.vimtex_syntax_enabled = 1
 vim.g.tex_flavor = "latex"
+
+require("date-time-inserter").setup({
+  date_format = "%Y-%m-%d",
+  time_format = "%H:%M:%SZ",
+  date_time_separator = "T",
+  presets = {},
+})
